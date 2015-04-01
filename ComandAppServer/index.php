7deb1b71d304fcaec236acd18f1c9a820a08932c
AlexBrowneP <pi.alexbrowne@gmail.com>
2015-04-02 01:10:15 +0200

    Server v3.0
    Añadida función genérica y parsers XML para reducir la carga a los
    clientes. A falta de pruebas.

--- ComandAppServer/index.php
+++ ComandAppServer/index.php
@@ -2,13 +2,18 @@
 
 /*
   PROTOCOLO DE COMUNICACIÓN
-  HTTP mediante GET únicamente.
+  HTTP mediante POST únicamente.
 
   1. Cada comunicación deberá tener al menos un parámetro "COM".
   Éste toma el valor de alguna de las siguientes opciones:
 
     MAIN - Devuelve el estado actual de la base de datos del servidor.
     No tiene parámetros.
+
+    GBAR - Todos los datos sobre un bar o bares.
+    Parámetros:
+    ID - Identificador/Identificadores(seguidos de una coma).
+    Salida : Documento XML con los datos de local. Si hay más de uno se concatenan.
 
     GLOC - Datos sobre el local. No de su carta ni de sus ofertas.
     Parámetros:
@@ -26,31 +31,15 @@
     Salida : Documento XML con los datos de las ofertas actuales. Si hay más de uno se concatenan.
  */
 
-if (isset($_GET["COM"]) && isset($_GET["ID"])) {
-    require("functions.php");
+if (isset($_POST["COM"])) {
+    include("functions.php");
     header('Content-type: application/xml');
-
-    $com = substr($_GET["COM"], 0, 4);
-    $idString = substr($_GET["ID"], 0, 100);
-    $idArray = sanitazeIdString($idString);
-
-    try {
-        if (strcmp($com, "GLOC") == 0) {
-            echo sendInfoLocal($idArray);
-        } elseif (strcmp($com, "GCAR") == 0) {
-            echo sendCarta($idArray);
-        } elseif (strcmp($com, "GOFE") == 0) {
-            echo sendOfertas($idArray);
-        } else {
-            exit;
-        }
-    } catch (PDOException $ex) {
-        echo "error: " . $ex->getMessage();
+    $com = substr($_POST["COM"], 0, 4);
+    
+    if(strcmp($com,"GLOC") == 0) {
+        sendResponse($_POST["MAIN"]);
     }
-} else if(isset($_GET["COM"]) && !isset($_GET["ID"])) {
-    require("functions.php");
-    header('Content-type: application/xml');
+ 
+} else if(isset($_GET["COM"]) && !isset($_GET["MAIN"])) {
 
-    $com = substr($_GET["COM"], 0, 4);
-    if (strcmp($com, "MAIN") == 0) echo sendMain();
 } else exit;
