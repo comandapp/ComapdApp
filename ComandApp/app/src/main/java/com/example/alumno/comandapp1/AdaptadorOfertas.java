f83e71263d952cecd3df01a7ce71e03bee97de5d
Victor <virubiotor@gmail.com>
2015-04-16 18:03:30 +0200

    cantidad
    asd

--- ComandApp/app/src/main/java/com/example/alumno/comandapp1/AdaptadorOfertas.java
+++ ComandApp/app/src/main/java/com/example/alumno/comandapp1/AdaptadorOfertas.java
@@ -1,7 +1,9 @@
 package com.example.alumno.comandapp1;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -46,6 +49,17 @@ public class AdaptadorOfertas extends ArrayAdapter<Oferta> {
             holder.nombre = (TextView)item.findViewById(R.id.lblNombreOferta);
             holder.precio=(TextView)item.findViewById(R.id.lblPrecioOferta);
             holder.descripcion=(TextView)item.findViewById(R.id.lblDescripcionOferta);
+
+            LinearLayout hl=(LinearLayout)v.findViewById(R.id.hLayout);
+            TextView tv=new TextView(context);
+            tv.setText("0");
+            RelativeLayout.LayoutParams adminTextViewLayoutParams = new RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            tv.setLayoutParams(adminTextViewLayoutParams);
+            tv.setGravity(Gravity.CENTER_VERTICAL|Gravity.RIGHT);
+            hl.addView(tv);
+
+
             ((LinearLayout)item.findViewById(R.id.LLayout)).setOnClickListener(new View.OnClickListener() {
 
                 @Override
@@ -55,19 +69,25 @@ public class AdaptadorOfertas extends ArrayAdapter<Oferta> {
                        for(int i=0;i<((ListView)context.findViewById(R.id.lo)).getChildCount();i++) {
                            View a=(LinearLayout)((ListView)context.findViewById(R.id.lo)).getChildAt(i).findViewById(R.id.LLayout);
                            if(a instanceof LinearLayout) {
-                               ((LinearLayout)((LinearLayout)a).findViewById(R.id.hLayout)).removeView(((LinearLayout)((LinearLayout)a).findViewById(R.id.hLayout)).getChildAt(2));
-                               ((LinearLayout)((LinearLayout)a).findViewById(R.id.hLayout)).removeView(((LinearLayout)((LinearLayout)a).findViewById(R.id.hLayout)).getChildAt(2));
+                               if(((LinearLayout)((LinearLayout)a).findViewById(R.id.hLayout)).getChildCount()!=3) {
+                                   ((LinearLayout) ((LinearLayout) a).findViewById(R.id.hLayout)).removeView(((LinearLayout) ((LinearLayout) a).findViewById(R.id.hLayout)).getChildAt(2));
+                                   ((LinearLayout) ((LinearLayout) a).findViewById(R.id.hLayout)).removeView(((LinearLayout) ((LinearLayout) a).findViewById(R.id.hLayout)).getChildAt(3));
+                               }
                            }
                        }
                        // ((ViewGroup)v.getParent().getParent()).getId();
                        // hl.removeView(aux);
+
                     LinearLayout hl=(LinearLayout)v.findViewById(R.id.hLayout);
+
                     Button but=new Button(context);
-                    but.setText("<<");
-                    hl.addView(but);
+                    but.setText("▲");
+                    but.setTextSize(40);
+                    hl.addView(but,2);
                     but=new Button(context);
-                    but.setText(">>");
-                    hl.addView(but);
+                    but.setText("▼");
+                    but.setTextSize(40);
+                    hl.addView(but,4);
                     notifyDataSetChanged();
                 }
             });
