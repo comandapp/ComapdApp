7d92344537fad1b06b6b79d93e53a8088fab4f05
Victor <virubiotor@gmail.com>
2015-05-05 16:33:35 +0200

    awe

--- ComandAppCliente/app/src/main/java/comandapp/comandappcliente/presentacion/actividades/MainActivity.java
+++ ComandAppCliente/app/src/main/java/comandapp/comandappcliente/presentacion/actividades/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         btnBuscarLocal = (Button)findViewById(R.id.btnBuscarLocal);
